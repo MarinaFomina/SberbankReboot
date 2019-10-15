11a3aff5bf0ef38da1ce4e1ea6a9c458ade94044
shevninka1974 <4a flarm v>
2019-10-15 22:57:18 +0700

    Task01 Add Exclamation marks

--- Task_01/src/main/java/kuzmina/Task_01.java
+++ Task_01/src/main/java/kuzmina/Task_01.java
@@ -2,6 +2,6 @@ package main.java.kuzmina;
 
 public class Task_01 {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+        System.out.println("Hello world!!");
     }
 }
