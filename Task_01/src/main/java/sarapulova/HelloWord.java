8e7051ec123512327335c643f7c065bd49fbbb9b
Ancha.lynx <ancha.lynx@gmail.com>
2019-10-16 23:25:23 +0700

    feature-01-sarapulova try to add new class

---
+++ Task_01/src/main/java/sarapulova/HelloWord.java
@@ -0,0 +1,7 @@
+package main.java.sarapulova;
+
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hekko world! Ann going to study java");
+    }
+}
