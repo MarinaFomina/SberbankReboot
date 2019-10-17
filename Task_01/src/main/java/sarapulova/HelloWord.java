0c769d30d4f30bd858d34159c8f9e6041ea06190
Ancha.lynx <ancha.lynx@gmail.com>
2019-10-17 23:55:49 +0700

    Revert "feature-01-sarapulova try to add new class"
    This reverts commit 8e7051ec

--- Task_01/src/main/java/sarapulova/HelloWord.java
+++
@@ -1,7 +0,0 @@
-package main.java.sarapulova;
-
-public class HelloWord {
-    public static void main(String[] args) {
-        System.out.println("Hekko world! Ann going to study java");
-    }
-}
