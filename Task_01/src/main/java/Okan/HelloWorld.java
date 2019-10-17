96fd8b0709821bd9ec747af05a04b61541b0beb5
O <OkanyES>
2019-10-17 21:08:34 +0700

    May First prg

---
+++ Task_01/src/main/java/Okan/HelloWorld.java
@@ -0,0 +1,13 @@
+package main.java.Okan;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!!");
+        System.out.println("TestNextRow!!");
+
+
+    }
+
+
+}
