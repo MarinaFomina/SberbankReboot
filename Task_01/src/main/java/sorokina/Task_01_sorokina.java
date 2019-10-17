ea31f87cf55db21037096812f987bcb5459e6fd6
SorokinaMargarita <margarita.sorokina@gmail.com>
2019-10-17 15:41:38 +0700

    bugfix-01-sorokina fix class name

--- Task_01/src/main/java/sorokina/Task_01_sorokina.java
+++ Task_01/src/main/java/sorokina/Task_01_sorokina.java
@@ -1,7 +1,8 @@
 package main.java.sorokina;
 
-public class task_01_sorokina {
+public class Task_01_sorokina {
     public static void main(String[] args) {
         System.out.println("Hello world!!!");
     }
 }
+
