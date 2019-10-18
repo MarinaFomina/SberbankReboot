82b28885a5ee2a1985758201f5e475b3829b3491
O <OkanyES>
2019-10-18 10:02:24 +0700

    May First prg

---
+++ Task_01/src/main/java/Okan/Calc.java
@@ -0,0 +1,14 @@
+package main.java.Okan;
+
+public class Calc {
+    public static void main(String[] args) {
+
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1]));
+
+
+    }
+
+}
---
+++ Task_01/src/main/java/Okan/Person.java
@@ -0,0 +1,19 @@
+package main.java.Okan;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    private void displayinfo () {
+        System.out.println("Name:" + name + " Age:" + age);
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
